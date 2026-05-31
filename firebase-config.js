fed0ed0292f39af43df344864040d14c217b1b0c
12Augustss <pk093529@gmail.com>
2026-05-31 15:33:30 +0700

    ffg

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Firebase config ของโปรเจกต์ klshdrug ใส่ให้แล้ว
+// ใช้กับ Firebase Realtime Database สำหรับ KLSH Drug Search
+window.KLSH_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBiWpgE865sJJreAfzCwUZLNQQwSWq303s",
+  authDomain: "klshdrug.firebaseapp.com",
+  databaseURL: "https://klshdrug-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "klshdrug",
+  storageBucket: "klshdrug.firebasestorage.app",
+  messagingSenderId: "1058315351435",
+  appId: "1:1058315351435:web:c4cda0813193661e4e2b8e"
+};
+
+// path ที่ใช้เก็บรายการยาใน Realtime Database
+window.KLSH_FIREBASE_DB_PATH = "klsh_drug_search/drugs";
